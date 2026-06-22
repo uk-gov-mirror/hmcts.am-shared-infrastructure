1dbee0abaaec3494f4953406cc2deb86c00f8e76
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-06-22 21:36:06 +0100

    DTSAM-1330.Change sampling size for am-perftest App Insights

--- appinsights.tf
+++ appinsights.tf
@@ -3,10 +3,8 @@ module "application_insights" {
   location = var.appinsights_location
   env      = var.env
   product  = var.product
-
-
+  sampling_percentage = var.sampling_percentage
   resource_group_name = azurerm_resource_group.rg.name
-
   common_tags = local.tags
 }
 
