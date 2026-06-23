d4105a67a3d5fdb45d0dcd3c70759dd36fa440ad
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2026-06-23 06:44:54 +0000

    Updating Terraform Formatting

--- appinsights.tf
+++ appinsights.tf
@@ -1,11 +1,11 @@
 module "application_insights" {
-  source   = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
-  location = var.appinsights_location
-  env      = var.env
-  product  = var.product
+  source              = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
+  location            = var.appinsights_location
+  env                 = var.env
+  product             = var.product
   sampling_percentage = var.sampling_percentage
   resource_group_name = azurerm_resource_group.rg.name
-  common_tags = local.tags
+  common_tags         = local.tags
 }
 
 moved {
